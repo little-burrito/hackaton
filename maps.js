4550ab788593d158c7110857073cb0464d4d6e20
David ödling <davidodling@me.com>
2014-02-08 22:09:21 +0100

    modified:   index.html 	new file:   libraries/mapbox/HeatLayer.js 	deleted:    libraries/mapbox/leaflet-src.js 	modified:   maps.js

--- maps.js
+++ maps.js
@@ -1,3 +1,4 @@
+
 var map = L.mapbox.map('map', 'examples.map-9ijuk24y')
     .setView([40, -74.50], 9);
 function markerrender ( )
@@ -5,15 +6,21 @@ function markerrender ( )
     if (navigator.geolocation) {
         map.locate();
     }
-    setTimeout(markerrender, 1000);
+    setTimeout(markerrender, 10000);
 };
+var geoJson = {};
 
-
+function extractgeos(){
+    for (var key in geoJson.features) {
+        console.log(geoJson.features[key].geometry.coordinates , "test");
+        // to do
+    }
+};
 // Once we've got a position, zoom and center the map
 // on it, and add a single marker.
 map.on('locationfound', function(e) {
     //map.fitBounds(e.getBounds());
-    var geoJson = {
+    geoJson = {
         type: 'FeatureCollection',
         features: [
             {type: "Feature",
@@ -54,13 +61,16 @@ map.on('locationfound', function(e) {
             }]
 
     };
+//    heat = L.heatLayer(e.latlng, {radius: 25});
     map.markerLayer.on('click', function(e) {
         e.layer.unbindPopup();
         window.open(e.layer.feature.properties.url);
     });
     map.markerLayer.setGeoJSON(geoJson);
     map.fitBounds(map.markerLayer.getBounds());
+    extractgeos();
 });
+
 markerrender();
 
 
@@ -70,3 +80,4 @@ map.on('locationerror', function() {
     alert('position could not be found');
 });
 
+map.Layers.addOverlay(heat);
