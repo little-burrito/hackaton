d78794d00e933d2519b101f437fa58fb8e626d37
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2014-02-09 01:05:52 +0100

    Click/swipe function to open/close nav bar

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
+	var height = $(window).height();
+	var navHeight = $('#navbar').outerHeight();
+	var swipeHeight = height-navHeight+'px';
+	console.log('swipeHeight: '+swipeHeight);
+	$('#swipable').css('top', swipeHeight);
 
-
-
-}
+	var swipable = document.getElementById('swipable');
+	$(swipable).click(function() {
+		console.log('click');
+		$('#swipable').animate({top:0});
+		Hammer(swipable).on('swipedown', function() {
+			console.log('swipe');
+			$('#swipable').animate({'top': swipeHeight});
+		});
+	});
+});
